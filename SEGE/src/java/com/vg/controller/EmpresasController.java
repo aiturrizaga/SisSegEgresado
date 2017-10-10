916244bb5b62b7ea28ed50a148f97a12e6897993
Alvaro <aiturrizaga@vallegrande.edu.pe>
2017-10-09 22:15:54 -0500

    New packcage created

---
+++ SEGE/src/java/com/vg/controller/EmpresasController.java
@@ -0,0 +1,10 @@
+package com.vg.controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+@Named(value = "empresasController")
+@Dependent
+public class EmpresasController {
+
+}
